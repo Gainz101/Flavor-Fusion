65e179c69c467678b4d714ace2060c7364d6ba3d
isaiahmunoz2002-tamu <isaiahmunoz2002@tamu.edu>
2023-11-25 00:58:56 -0600

    BARD

--- bard.py
+++ bard.py
@@ -1,13 +1,9 @@
 from bardapi import Bard
 
 # Replace API_KEY with your actual API key
-#API_KEY = "cggGgdKpEjFtDiPxOpUKdP86OMbI9X2_Ufunoho4vXxkXFMxg2WPMjzcaNz_0mDMgIwjHQ."
-#API_KEY = "AIzaSyBqX_7lAyUaAd_bPWhf2OBo8SB-eJnqzYI"
-API_KEY = "AIzaSyBqX_7lAyUaAd_bPWhf2OBo8SB-eJnqzYI."
-
+API_KEY = "dQjZqQzR9M6HH_KhYMoag7vDze3qTkG_nizXGID8HuRPEU2IqX-bXks9SqlB1DMnzGaudA."
 
 bard = Bard(token=API_KEY)
 
-input_query = "What is the capital of France?"
-result = bard.retrieve(input_query)
+result = bard.get_answer("What is the capital of France?")
 print(result)
--- bard2.py
+++ bard2.py
@@ -1,7 +1,7 @@
 import google_bard
 
 # Replace "YOUR_API_KEY" with the actual API Key obtained earlier
-API_KEY = AIzaSyBqX_7lAyUaAd_bPWhf2OBo8SB-eJnqzYI
+API_KEY = dQjZqVp3bgmvc_PYzRXDmOfV47vyUClUQW6hS95BF2GT5uNVphCUqqwbl3zQ6M7qJjY5zg
 
 def main():
 	query = "What is the meaning of life?"
